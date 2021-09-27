aef48bc0769b5f7694b3cf3f1d61f00932e51d2d
MauroSSebastian <sntorrez9@gmail.com>
2021-09-27 11:42:00 -0300

    add router test get

--- components/mutant/Routers.js
+++ components/mutant/Routers.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('./Controllers');
 
 router.post('/', controller.mutant);
+router.get('/test', (req,res)=>res.json({ok:true}));
 
 module.exports = router;
